8bb5871e67affa7f2de99918c865b8556da62c42
Robin Mahto <58412832+robinmahto@users.noreply.github.com>
2023-10-25 10:22:39 +0000

    setup routing and tailwindcss

---
+++ src/pages/Browse.jsx
@@ -0,0 +1,8 @@
+
+const Browse = () => {
+  return (
+    <div className="text-3xl font-bold underline">Browse</div>
+  )
+}
+
+export default Browse
---
+++ src/pages/Login.jsx
@@ -0,0 +1,8 @@
+
+const Login = () => {
+  return (
+    <div className="w-full h-full ">Login</div>
+  )
+}
+
+export default Login
